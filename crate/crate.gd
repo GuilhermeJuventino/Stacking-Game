384947fbbedbfcfc1b62ba728db5fdde8c36e180
Guilherme Juventino <guilhermejuventino@gmail.com>
2023-09-23 14:49:17 -0300

    Still working on stacking the crates...

--- crate/crate.gd
+++ crate/crate.gd
@@ -2,20 +2,26 @@ extends RigidBody2D
 
 var stacked: bool = false
 
-@onready var collision_2: CollisionShape2D = $Collision2
+@onready var collision_2: CollisionShape2D = $CrateBottom/Collision2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_freeze_mode(RigidBody2D.FREEZE_MODE_KINEMATIC)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	pass
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	#var bodies = get_colliding_bodies()
+	pass
+	
 func stack():
+	print("Stack() was called")
 	
-	
-	if stacked:
-		sleeping = true
-		set_freeze_enabled(sleeping)
+	Game.crates.append(self)
+	stacked = true
+	sleeping = true
+	set_freeze_enabled(sleeping)
+
+
+func _on_crate_bottom_body_entered(_body):
+	if !stacked:
+		call_deferred("stack")
